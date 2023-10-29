ea4290c513625215b8e3b7cfc0da836796507bd7
Cedrick Garcia <icedgarcia@gmail.com>
2023-10-29 15:48:46 +0800

    merge all users in 1 table

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -10,21 +10,21 @@
             </a>
           </li>
           <!-- USERS -->
-          <li class="nav-item">
-                                <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-1" aria-controls="submenu-1"><i class="fas fa-users"></i>  Users</a>
-                                <div id="submenu-1" class=" submenu sidebar-collapse" style="">
-                                    <ul class="nav flex-column">
+          <!-- <li class="nav-item"> -->
+                                <!-- <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-1" aria-controls="submenu-1"><i class="fas fa-users"></i>  Users</a> -->
+                                <!-- <div id="submenu-1" class=" submenu sidebar-collapse" style="">
+                                    <ul class="nav flex-column"> -->
                                         <li class="nav-item">
                                           
-                                            <a class="nav-link" href="owners.php">Owners</a>
+                                            <a class="nav-link" href="users.php"><i class="fas fa-users"></i>  Users</a>
                                         </li>
-                                        <li class="nav-item">
+                                        <!-- <li class="nav-item">
                                             <a class="nav-link" href="tenants.php">Tenants </a>
                                         </li>
-                                       
-                                    </ul>
-                                </div>
-                            </li>
+                                        -->
+                                    <!-- </ul> -->
+                                <!-- </div> -->
+                            <!-- </li> -->
                             <!-- CONCOURSE -->
 
                             <li class="nav-item">
@@ -44,7 +44,7 @@
                             </li>
 <!-- VERIFICATIONS -->
                             <li class="nav-item">
-                                <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-2" aria-controls="submenu-2"><i class="fas fa-map-marked-alt"></i> Verifications</a>
+                                <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-2" aria-controls="submenu-2"><i class="fa-solid fa-file-circle-check"></i>Verifications</a>
                                 <div id="submenu-3" class=" submenu sidebar-collapse" style="">
                                     <ul class="nav flex-column">
                                         <li class="nav-item">
