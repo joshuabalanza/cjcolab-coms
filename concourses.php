0e2ad7393a4f3d184286b694b38dcb6712000917
Cedrick Garcia <icedgarcia@gmail.com>
2023-11-01 14:33:45 +0800

    display buttons depending on utype and status

--- concourses.php
+++ concourses.php
@@ -2,14 +2,12 @@
 <!-- ***START SESSION**** -->
 <!-- ******************** -->
 <?php
-session_name("user_session");
+   session_name("user_session");
 session_start();
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 require('includes/dbconnection.php');
 ?>
-
-
 <!-- ******************** -->
 <!-- ***** PHP CODE ***** -->
 <!-- ******************** -->
@@ -34,7 +32,6 @@ if ($verificationResult && mysqli_num_rows($verificationResult) > 0) {
     $verificationStatus = $verificationData['status'];
 }
 ?>
-
 <!-- ******************** -->
 <!-- **** START HTML **** -->
 <!-- ******************** -->
@@ -42,46 +39,42 @@ if ($verificationResult && mysqli_num_rows($verificationResult) > 0) {
 include('includes/header.php');
 include('includes/nav.php');
 ?>
-
-
-<section style= "margin-top:74px;">
-<?php
-echo 'Hi, ' . $_SESSION['uname'] . ' (' . $_SESSION['utype'] . ')';
-echo $utype;
+<section style= "margin-top:75px;">
+   <?php
+//    echo 'Hi, ' . $_SESSION['uname'] . ' (' . $_SESSION['utype'] . ')';
+// echo $utype;
 ?>
- 
- 
- 
- <div class="text-center">
-     <?php if ($verificationStatus === 'approved' && $utype === 'Owner'): ?>
-        <a href="concourse_add.php">
-            <button class="btn-sm btn btn-success">Add a Concourse</button>
-        </a>
-        <?php elseif ($verificationStatus === 'approved' && $utype === 'Tenant'): ?>
-            <a href="tenant-apply-space.php">
-                <button class="btn-sm btn btn-success">Apply For Space</button>
-                </a>
-               <?php else: ?>
-                <!-- You need to verify your account -->
-                
-                <!-- ************************************ -->
-                <!-- *******TO BE EDITED    ****** -->
-                <!-- ************************************ -->
-                <h1>
-                    spaces, rents, assigned spaces will be here
-                    
-                </h1>
-                <a href="verification_account.php">
-                    <button class="btn-sm btn btn-success">Verify Account to access concourses</button>
-                </a>
-                <?php endif; ?>
-            </div>
-            
-            
-            
-        </section>
-            
-            
-            
-            
-            <?php include('includes/footer.php'); ?>
+
+
+   <div class="text-center">
+<!-- ********************************************************************** -->
+<!-- **** CTA BUTTON DISPLAY DEPENDING ON USER TYPE AND ACCOUNT STATUS **** -->
+<!-- ********************************************************************** -->
+
+<!-- OWNER -->
+      <?php if ($verificationStatus === 'approved' && $utype === 'Owner'): ?>
+      <h3>Your Concourse</h3>
+      <a href="concourse_add.php">
+      <button class="btn-sm btn btn-success">Add a Concourse</button>
+      </a>
+      <?php elseif ($verificationStatus === 'rejected' && $utype === 'Owner'): ?>
+      <a href="verification_account.php">
+      <button class="btn-sm btn btn-success">Verify Account to add concourses</button>
+      </a>
+
+      <!-- TENANT -->
+      <?php elseif ($verificationStatus === 'approved' && $utype === 'Tenant'): ?>
+      <a href="tenant-apply-space.php">
+      <button class="btn-sm btn btn-success">Apply For Space</button>
+      </a>
+      <?php else: ?>
+      <a href="verification_account.php">
+      <button class="btn-sm btn btn-success">Verify Account to apply for Space</button>
+      </a>
+      <?php endif; ?>
+   </div>
+   <div class= "">
+      <h3>Concourses</h3>
+   </div>
+</section>
+<?php include('includes/footer.php'); ?>
