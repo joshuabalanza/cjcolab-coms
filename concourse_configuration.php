495d6622fc53eed52105718d385558aa85091c78
Cedrick Garcia <icedgarcia@gmail.com>
2023-11-08 17:38:32 +0800

    commiting for savepoint

--- concourse_configuration.php
+++ concourse_configuration.php
@@ -20,24 +20,27 @@ if (isset($_GET['concourse_id'])) {
     $concourseQuery = "SELECT * FROM concourse_verification WHERE concourse_id = $concourse_id";
     $concourseResult = mysqli_query($con, $concourseQuery);
 
+    // Form Data
+
+
     // Check if a POST request was made to update Concourse details
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $concourse_id = $_POST['concourse_id'];
 
         // Retrieve form data
-        $total_area = $_POST['total_area'];
-        $total_spaces = $_POST['spaces'];
-        $location = $_POST['location'];
+        // $total_area = $_POST['concourse_total_area'];
+        // $total_spaces = $_POST['spaces'];
+        // $location = $_POST['location'];
         // $contract_of_lease = $_FILES['contract_of_lease']['name']; // Handle file upload.
-        $feedback = $_POST['feedback'];
+        // $feedback = $_POST['feedback'];
 
         // Update the database with the new data
         $updateQuery = "UPDATE concourse_verification SET
-                concourse_total_area = '$total_area',
-                total_spaces = '$total_spaces',
-                location = '$location',
-                contract_of_lease = '$contract_of_lease',
-                feedback = '$feedback'
+                -- concourse_total_area = '$total_area',
+                -- total_spaces = '$total_spaces',
+                -- location = '$location',
+                -- contract_of_lease = '$contract_of_lease',
+                -- feedback = '$feedback'
                 WHERE concourse_id = $concourse_id";
 
         // Execute the query
@@ -59,8 +62,7 @@ include('includes/header.php');
 include('includes/nav.php');
 ?>
 
-<section class="concourse-configuration" style="margin-top:80px;   display: flex;
-">
+<section class="concourse-configuration" style="margin-top:80px;   display: flex; justify-content:space-evenly;">
 
   
         <div class="concourse-details">
@@ -75,12 +77,12 @@ include('includes/nav.php');
         echo '<h5 class="card-title">Concourse Map</h5>';
         echo '<a href="index.php" class="card-title">';
         echo '<button>Manage Spaces</button>';
-        echo ' <a>';
+        echo ' </a>';
         echo '</div>';
+        echo '<h5 class="card-title">' . $concourseData['concourse_name'] . '</h5>';
         echo '<div class="card">';
         echo '<img src="/COMS/uploads/' . $concourseData['concourse_map'] . '" class="card-img-top smaller-image" alt="Concourse Map" style="width: 200px; height: 200px;">';
         echo '<div class="card-body">';
-        echo '<h5 class="card-title">' . $concourseData['concourse_name'] . '</h5>';
         echo '<p class="card-text">Concourse ID: ' . $concourseData['concourse_id'] . '</p>';
         echo '<p class="card-text">Owner ID: ' . $concourseData['owner_id'] . '</p>';
         // echo '<p class="card-text">Concourse Name: ' . $concourseData['owner_name'] . '</p>';
@@ -96,27 +98,67 @@ include('includes/nav.php');
 ?>
 </div>
 
+<div>
+    
+<h5 class="card-title">Concourse Featured Image</h5>
+<button id="changeImageBtn">Change</button>
+<input type="file" id="concourseImageInput" style="display: none;">
+<img src="/COMS/uploads/<?php echo $concourseData['concourse_map']; ?>" id="concourseImage" class="card-img-top smaller-image" alt="Concourse Map" style="width: 200px; height: 200px;">
+<!-- <img src="/COMS/uploads/'. <?php echo  $concourseData['concourse_map'] ?> . ' " alt="MAP"> -->
 
-    <div class="edit-concourse-form">
+<!-- <img src="/COMS/uploads/' . $concourseData['concourse_map'] . '" class="card-img-top smaller-image" alt="Concourse Map" style="width: 200px; height: 200px;"> -->
+
+<div class="edit-concourse-form">
         <h3>Edit Concourse Details</h3>
-        <form method="post" action="concourse_configuration.php?concourse_id=<?php echo $concourse_id; ?>">
-            <label for="total_area">Total Area (sq ft):</label>
-            <input type="text" name="total_area" value="<?php echo $concourseData['concourse_total_area']; ?>"><br>
-
-            <label for="total_spaces">Total Spaces:</label>
-            <input type="text" name="total_spaces" value="<?php echo $concourseData['spaces']; ?>"><br>
-
-            <!-- <label for="location">Location (City/Postal Code/Barangay):</label>
-            <input type="text" name="location" value="<?php echo $concourseData['location']; ?>"><br> -->
-
-            <label for="contract_of_lease">Contract of Lease:</label>
-            <input type="file" name="contract_of_lease"><br>
+        <form method="post" action="concourse_configuration.php?concourse_id=<?php echo $concourse_id; ?>" enctype="multipart/form-data">
+    <label for="total_area">Total Area (sq ft):</label>
+    <input type="text" name="total_area" value="<?php echo $concourseData['concourse_total_area']; ?>"><br>
 
 
-            <input type="submit" value="Save">
-        </form>
+    <!-- HINDI KO ALAM KUNG DAPAT BANG BAGUHIN YUNG MGA NAKA COMMENT  -->
+    <!-- <label for="total_spaces">Total Spaces:</label>
+    <input type="text" name="total_spaces" value="<?php echo $concourseData['spaces']; ?>"><br> -->
+
+    <!-- <label for="location">Location (City/Postal Code/Barangay):</label> -->
+    <!-- <input type="text" name="location" value="<?php echo $concourseData['location']; ?>"><br> -->
+
+    <label for="contract_of_lease">Contract of Lease:</label>
+    <input type="file" name="contract_of_lease"><br>
+
+    <label for="concourse_image">Concourse Image:</label>
+    <input type="file" name="concourse_image"><br>
+
+    <input type="submit" value="Save">
+</form>
+
     </div>
 
 </section>
 
 <?php include('includes/footer.php'); ?>
+
+
+<script>
+    // Get the button and input element
+    const changeImageBtn = document.getElementById('changeImageBtn');
+    const concourseImageInput = document.getElementById('concourseImageInput');
+    const concourseImage = document.getElementById('concourseImage');
+
+    // Add an event listener to the "Change" button
+    changeImageBtn.addEventListener('click', () => {
+        concourseImageInput.click();
+    });
+
+    // Add an event listener to the input file element
+    concourseImageInput.addEventListener('change', (event) => {
+        const selectedFile = event.target.files[0];
+        if (selectedFile) {
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                // Display the selected image as a preview
+                concourseImage.src = e.target.result;
+            };
+            reader.readAsDataURL(selectedFile);
+        }
+    });
+</script>
