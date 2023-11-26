b31a75f664d6c9d2c50c5a1edc8b4c0366769981
Mayki Neri <maykineri@gmail.com>
2023-11-26 23:56:50 +0800

    auto notif 4 bill

--- process_create_bill.php
+++ process_create_bill.php
@@ -4,6 +4,11 @@ session_start();
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 require('includes/dbconnection.php');
+require 'vendor/autoload.php'; // Include PHPMailer autoload
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 error_log('process_create_bill.php accessed.');
 error_log('POST data: ' . print_r($_POST, true));
 
@@ -20,12 +25,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['create_bill'])) {
         exit();
     }
 
-    // Get tenant_name and space_bill from space table
-    $getTenantQuery = "SELECT space_tenant, space_bill FROM space WHERE space_id = '$spaceId'";
-    $tenantResult = $con->query($getTenantQuery);
+    // Get tenant_name, space_bill, and tenant_email from space table
+    $getTenantQuery = "SELECT space_tenant, space_bill FROM space WHERE space_id = ?";
+    $stmt = $con->prepare($getTenantQuery);
+    $stmt->bind_param("s", $spaceId);
+    $stmt->execute();
+    $tenantResult = $stmt->get_result();
 
     if ($tenantResult === false) {
-        echo json_encode(['error' => 'Error retrieving tenant data: ' . $con->error]);
+        echo json_encode(['error' => 'Error retrieving tenant data: ' . $stmt->error]);
         exit();
     }
 
@@ -34,22 +42,52 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['create_bill'])) {
         $tenantName = $tenantRow['space_tenant'];
         $spaceBill = $tenantRow['space_bill'];
 
-        // Calculate total bill
-        $total = $electric + $water + $spaceBill;
+        // Get tenant_email from the user table
+        $getTenantEmailQuery = "SELECT uemail FROM user WHERE uname = ? AND utype = 'tenant'";
+        $stmt = $con->prepare($getTenantEmailQuery);
+        $stmt->bind_param("s", $tenantName);
+        $stmt->execute();
+        $emailResult = $stmt->get_result();
 
-        // Insert into the bill table
-        $insertBillQuery = "INSERT INTO bill (space_id, tenant_name, electric, water, space_bill, total, due_date, created_at) 
-                            VALUES ('$spaceId', '$tenantName', '$electric', '$water', '$spaceBill', '$total', DATE_ADD(NOW(), INTERVAL 7 DAY), NOW())";
+        if ($emailResult->num_rows > 0) {
+            $emailRow = $emailResult->fetch_assoc();
+            $tenantEmail = $emailRow['uemail'];
 
-        $insertResult = $con->query($insertBillQuery);
+            // Calculate total bill
+            $total = $electric + $water + $spaceBill;
 
-        if ($insertResult === true) {
-            // Assuming $concourseId is set in view_concourse.php
-            // Redirect back to the view_concourse.php page or another appropriate page
-            header('Location: view_concourse.php?concourse_id=' . $concourseId);
-            exit();
+            // Insert into the bill table using prepared statements
+            $insertBillQuery = "INSERT INTO bill (space_id, tenant_name, tenant_email, electric, water, space_bill, total, due_date, created_at, notified) 
+                                VALUES (?, ?, ?, ?, ?, ?, ?, DATE_ADD(NOW(), INTERVAL 7 DAY), NOW(), 'not notified')";
+
+            $stmt = $con->prepare($insertBillQuery);
+            $stmt->bind_param("sssssss", $spaceId, $tenantName, $tenantEmail, $electric, $water, $spaceBill, $total);
+
+            if ($stmt->execute()) {
+                // Notify the tenant via email
+                if (sendEmailToTenant($tenantEmail, $tenantName, $total)) {
+                    // Update notified status to 'notified'
+                    $updateNotifiedQuery = "UPDATE bill SET notified = 'notified' WHERE space_id = ? AND tenant_name = ?";
+                    $stmt = $con->prepare($updateNotifiedQuery);
+                    $stmt->bind_param("ss", $spaceId, $tenantName);
+                    $stmt->execute();
+                }
+
+                // Update space_bill in the space table
+                $updateSpaceBillQuery = "UPDATE space SET space_bill = ? WHERE space_id = ?";
+                $stmt = $con->prepare($updateSpaceBillQuery);
+                $stmt->bind_param("ss", $total, $spaceId);
+                $stmt->execute();
+
+                // Redirect back to the view_concourse.php page or another appropriate page
+                header('Location: view_concourse.php?concourse_id=' . $concourseId);
+                exit();
+            } else {
+                echo json_encode(['error' => 'Error creating bill: ' . $stmt->error]);
+                exit();
+            }
         } else {
-            echo json_encode(['error' => 'Error creating bill: ' . $con->error]);
+            echo json_encode(['error' => 'Tenant email not found.']);
             exit();
         }
     } else {
@@ -59,5 +97,38 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['create_bill'])) {
 } else {
     echo json_encode(['error' => 'Invalid request.']);
     exit();
+}
+
+// Function to send an email to the tenant
+function sendEmailToTenant($email, $tenantName, $total)
+{
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com'; // Your SMTP server
+        $mail->SMTPAuth = true;
+        $mail->Username = 'coms.system.adm@gmail.com'; // Your Gmail email address
+        $mail->Password = 'wdcbquevxahkehla'; // Your Gmail password
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+
+        //Recipients
+        $mail->setFrom('coms.system.adm@gmail.com', 'Concessionaire Monitoring Operation System');
+        $mail->addAddress($email, $tenantName);     // Add a recipient
+
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = 'New Bill Notification';
+        $mail->Body    = "Dear $tenantName, <br><br>Your new bill is ready. The total amount is $total. <br><br>Regards, <br>Your Landlord";
+
+        $mail->send();
+
+        return true; // Email sent successfully
+    } catch (Exception $e) {
+        echo json_encode(['error' => 'Email could not be sent. Mailer Error: ' . $mail->ErrorInfo]);
+        return false; // Email sending failed
+    }
 }
 ?>
