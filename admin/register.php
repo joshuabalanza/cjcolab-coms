cde389ca3131af65dc2e5259e7b43e06f46aab75
Cedrick Garcia <icedgarcia@gmail.com>
2023-11-27 06:41:35 +0800

    register form, copy style from PR#38

--- admin/register.php
+++ admin/register.php
@@ -91,20 +91,169 @@ if (isset($_POST['aregister'])) {
 <?php include('includes/header.php')?>
 <?php include('includes/nav.php')?>
 <!-- Start Create Account -->
-<section class="register-form">
-<div class="container pt-5">
-    <h2 class="text-center">Create an Account</h2>
-    <div class="row mt-4 mb-4" >
-        <div class="col-md-6 offset-md-3">
-            <form action="" class="shadow-lg p-4" method="POST">
-                <p><?php
-                if (isset($error_message)) {
-                    echo $error_message;
-                }
-?></p>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Poppins',sans-serif;
+}
+body{
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  background-color: #9b593c;
+}
+.container{
+  max-width: 700px;
+  width: 100%;
+  background-color: #fff;
+  padding: 25px 30px;
+  border-radius: 5px;
+  box-shadow: 0 5px 10px rgba(0,0,0,0.15);
+}
+.container .title{
+  font-size: 25px;
+  font-weight: 500;
+  position: relative;
+}
+.container .title::before{
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  height: 3px;
+  width: 30px;
+  border-radius: 5px;
+  background-color: #9b593c;
+}
+.content form .user-details{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 20px 0 12px 0;
+}
+form .user-details .input-box{
+  margin-bottom: 15px;
+  width: calc(100% / 2 - 20px);
+}
+form .input-box span.details{
+  display: block;
+  font-weight: 500;
+  margin-bottom: 5px;
+}
+.user-details .input-box input{
+  height: 45px;
+  width: 100%;
+  outline: none;
+  font-size: 16px;
+  border-radius: 5px;
+  padding-left: 15px;
+  border: 1px solid #ccc;
+  border-bottom-width: 2px;
+  transition: all 0.3s ease;
+}
+.user-details .input-box input:focus,
+.user-details .input-box input:valid{
+  border-color: #393E46;
+}
+ form .use-details .use-title{
+  font-size: 20px;
+  font-weight: 500;
+ }
+ form .category{
+   display: flex;
+   width: 80%;
+   margin: 14px 0 ;
+   justify-content: space-between;
+ }
+ form .category label{
+   display: flex;
+   align-items: center;
+   cursor: pointer;
+ }
+ form .category label .dot{
+  height: 18px;
+  width: 18px;
+  border-radius: 50%;
+  margin-right: 10px;
+  background: #d9d9d9;
+  border: 5px solid transparent;
+  transition: all 0.3s ease;
+}
+ #dot-1:checked ~ .category label .one,
+ #dot-2:checked ~ .category label .two,
+ #dot-3:checked ~ .category label .three{
+   background: #393E46;
+   border-color: #d9d9d9;
+ }
+ form input[type="radio"]{
+   display: none;
+ }
+ form .button{
+   height: 45px;
+   margin: 35px 0
+ }
+ form .button input{
+   height: 100%;
+   width: 100%;
+   border-radius: 5px;
+   border: none;
+   color: #fff;
+   font-size: 18px;
+   font-weight: 500;
+   letter-spacing: 1px;
+   cursor: pointer;
+   transition: all 0.3s ease;
+   background-color: #9b593c;
+ }
+ form .button input:hover{
+  /* transform: scale(0.99); */
+  background-color: #c19f90;
+  }
+ @media(max-width: 584px){
+ .container{
+  max-width: 100%;
+}
+form .user-details .input-box{
+    margin-bottom: 15px;
+    width: 100%;
+  }
+  form .category{
+    width: 100%;
+  }
+  .content form .user-details{
+    max-height: 300px;
+    overflow-y: scroll;
+  }
+  .user-details::-webkit-scrollbar{
+    width: 5px;
+  }
+  }
+  @media(max-width: 459px){
+  .container .content .category{
+    flex-direction: column;
+  }
+}
+   </style>
+<!-- Start Create Account -->
+<div class="container">
+    <div class="title">Registration</div>
+    <div class="content" >
+        <form action="" method="POST">
+            <p>
+                <?php
+                    if (isset($error_message)) {
+                        echo $error_message;
+                    }
+?>
+            </p>
 
-    <div class="form-group form">
-        <!-- <label for="usertype" class= ""> <i class="fa-solid fa-users"></i>User Type</label>
+                <!-- <div class="form-group form">
+                    <label for="usertype" class= ""> <i class="fa-solid fa-users"></i>User Type</label>
                     <select class="form-control" id="usertype" name="usertype">
                         
                     <option selected="true" disabled="true" >--SELECT USER TYPE--</option>
@@ -112,39 +261,49 @@ if (isset($_POST['aregister'])) {
                         <option value="Tenant">Tenant</option>
                     </select>
                 </div> -->
-                <div class="form-group form">
-                <input type="text" class="form-control form-input" name="aname"id="aname" autocomplete="off" placeholder="">
-                <label for="aname" class="form-label">
-                <i class="fa-solid fa-user"></i>    
-                Name</label>
-            </div>
-                <div class="form-group form">
-                    <input type="text" class="form-control form-input" name="aemail"id="aemail" autocomplete="off" placeholder="">
-                <label for="aemail" class="form-label">
-                    <i class="fa-solid fa-envelope"></i>    
-                Email</label>
-            </div>
-                <div class="form-group form">
-                <input type="password" class="form-control form-input" name="apassword"id="apassword" placeholder="">
-                <label for="apassword" class="form-label">
-                    <i class="fa-solid fa-key"></i>
-                Password</label>
+            <div class="user-details">
+                <div class="input-box">
+                    <span for="aname" class="details">Name</span>
+                    <input type="text" 
+                           name="aname"
+                           id="aname" 
+                           autocomplete="off" 
+                           placeholder="Enter your name"
+                           required>
                 </div>
-                <div class="form-group form">
-                    <input type="password" class="form-control form-input" name="confirm_password" id="confirm_password" placeholder="">
-                    <label for="apassword" class="form-label">
-                        <i class="fa-solid fa-key"></i>
-                        
-                        Confirm Password</label>
 
-                    </div>
-                    <button type="submit" class="btn btn-danger mt-3 btn-block shadow-sm font-weight-bold" name="aregister">Sign Up</button>
-            </form>
-        </div>
+                <div class="input-box">
+                    <span for="aemail" class="details">Email</span>
+                    <input type="text" 
+                           name="aemail"
+                           id="aemail" 
+                           autocomplete="off" 
+                           placeholder="Enter your email"
+                           required>
+                </div>
+
+                <div class="input-box">
+                    <span for="apassword" class="details">Password</span>
+                    <input type="password" 
+                           name="apassword"
+                           id="apassword" 
+                           placeholder="Enter your password"
+                           required>
+                </div>
+
+                <div class="input-box">
+                    <span for="apassword" class="details">Confirm Password</span>
+                    <input type="password" 
+                           name="confirm_password" 
+                           id="confirm_password" 
+                           placeholder="Confirm your password"
+                           required>
+                </div>
+            </div>
+
+            <div class="button">
+            <input type="submit" name="aregister">
+            </div>
+        </form>
     </div>
 </div>
-</section>
-<!-- End Create Account -->
-
-
-<?php include('includes/footer.php')?>
