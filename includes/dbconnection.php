ee3fb41a4be09d4fe5045fe41003dff83100e220
cjru2 <joyruayana@gmail.com>
2023-12-09 21:24:20 +0800

    live db

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,10 +1,10 @@
 <?php
 
 // For Local
-$con = mysqli_connect("localhost", "root", "", "test_coms4");
+// $con = mysqli_connect("localhost", "root", "", "test_coms4");
 
 //For Live
-// $con = mysqli_connect("localhost", "u556873115_coms", "123!@#Prodcoms", "u556873115_live_coms");
+$con = mysqli_connect("localhost", "u556873115_coms", "123!@#Prodcoms", "u556873115_live_coms");
 
 // print_r("Connected");
 if(mysqli_connect_errno()) {
